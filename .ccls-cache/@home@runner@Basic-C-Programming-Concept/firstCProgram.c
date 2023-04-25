1fc7791197a85004ee374208b22aecbb84fa6eaf
AFIFAHAMED <5308510-AFIFAHAMED@users.noreply.replit.com>
2023-04-25 18:40:10 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Basic-C-Programming-Concept/firstCProgram.c
@@ -0,0 +1,18 @@
+
+//Head
+#include <stdio.h>
+
+//int here is the datatype
+//main() is the function 
+//printf is a build-in function 
+//printf function is coming from stdio.h header file
+//stdio - Standard input and output
+//at the end of the program return 0
+
+//body
+int main(){
+
+    //code
+    printf("Hello Afif");
+    return 0;
+}
---
+++ .ccls-cache/@home@runner@Basic-C-Programming-Concept/variable3.c
@@ -0,0 +1,34 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+int main() {
+
+  // integer
+  int a = 25;
+  int c = 25;
+
+  int sum = a + c;
+
+  // float
+  float b = 5.43;
+
+  // charater
+  char A = 'a';
+
+  // boolean
+  bool t = true;
+  printf("Print Integer using %%d\n");
+  printf("Sum of two numbers is: %d", sum);
+  printf("\n");
+
+  // to print float value use %f
+  // to specify how many digits we want to show use %0.numOFDigits-f
+  // Example: %0.3f -> 0.111digits
+  printf("Print Float value\n");
+    printf("")
+
+  char name[] = "This is a string";
+  printf("%s", name);
+
+  return 0;
+}
