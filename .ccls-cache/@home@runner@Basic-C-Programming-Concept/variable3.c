e5e6ab9725b53b2e2f18b92caa291d225c7a25fb
Afif718 <afifahmad718@gmail.com>
2023-04-25 19:18:53 +0000

    Third Commit

--- .ccls-cache/@home@runner@Basic-C-Programming-Concept/variable3.c
+++ .ccls-cache/@home@runner@Basic-C-Programming-Concept/variable3.c
@@ -13,10 +13,11 @@ int main() {
   float b = 5.43;
 
   // charater
-  char A = 'a';
+  char A = 'X';
 
   // boolean
   bool t = true;
+
   printf("Print Integer using %%d\n");
   printf("Sum of two numbers is: %d", sum);
   printf("\n");
@@ -24,8 +25,13 @@ int main() {
   // to print float value use %f
   // to specify how many digits we want to show use %0.numOFDigits-f
   // Example: %0.3f -> 0.111digits
-  printf("Print Float value\n");
+  printf("Print Float value---> ");
   printf("%0.3f", b);
+  printf("\n");
+
+  // to print a char use %c
+  printf("This is a char ---> %c", A);
+  printf("\n");
 
   char name[] = "This is a string";
   printf("%s", name);
