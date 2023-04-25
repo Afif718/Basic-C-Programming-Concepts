1310febc27f87ec1309cc906e7e4f177657b5bfa
Afif718 <39927237+Afif718@users.noreply.github.com>
2023-04-25 19:54:48 +0000

    Module1

---
+++ .ccls-cache/@home@runner@Basic-C-Programming-Concept/Module 1@Print2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// here is main is a buil-in function, without it program can't run
+
+int Hello() {
+
+  // here printf() is a build-in function
+  // to use a build-in function we need to add the header file
+  // for printf() function, header file is in <stdio.h>
+  // so we need to add the header file to get the output
+
+  // to get output in new line use \n
+  // to get tab use \t
+  // to comment use \\
+
+  printf("Hellooo Afif\n");
+  printf("Hello Modhu!!!\n");
+  printf("Hello Anish!!!\n");
+  printf("Hello UCSI\nHello Dr Javed\t\t\t");
+
+  return 0;
+}
