adc60cca8e55383f70d66e21b9ec0ee4f6a4571e
Afif718 <39927237+Afif718@users.noreply.github.com>
2023-05-25 10:00:45 +0000

    Update

--- Module_7/array_input_output1.c
+++ Module_7/array_input_output1.c
@@ -5,12 +5,14 @@ int main(){
     //declare an array
     int arr[5];
 
+    //take input
     for(int i=0; i<5; i++){
         scanf("%d", &arr[i]);
     }
 
     printf("\n");
 
+    //print the array
     printf("Array output\n");
     for(int i=0; i<5; i++){
         printf("%d\n", arr[i]);
